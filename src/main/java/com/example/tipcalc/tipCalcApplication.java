406892f464c15f92c39418bf9ec6950d4dabaee3
sunwar01 <sunwar01@easv365.dk>
2022-10-04 12:29:59 +0200

    final

--- src/main/java/com/example/tipcalc/tipCalcApplication.java
+++ src/main/java/com/example/tipcalc/tipCalcApplication.java
@@ -1,23 +1,37 @@
 package com.example.tipcalc;
 
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+//import java.io.IOException;
+
 
 public class tipCalcApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(tipCalcApplication.class.getResource("tipCalc.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("tipCalc");
-        stage.setScene(scene);
-        stage.show();
-    }
+ @Override
+ public void start(Stage stage) throws Exception {
+         Parent root =
+                 FXMLLoader.load(getClass().getResource("tipCalc.fxml"));
 
-    public static void main(String[] args) {
-        launch();
-    }
-}
+         Scene scene = new Scene(root);
+         stage.setTitle("Tip Calculator");
+         stage.setScene(scene);
+         stage.show();
+         }
+
+         public static void main(String[] args) {
+
+         launch(args);
+         }
+ }
+
+
+
+
+
+
+
+
--- src/main/java/com/example/tipcalc/tipCalcController.java
+++ src/main/java/com/example/tipcalc/tipCalcController.java
@@ -1,32 +1,86 @@
 package com.example.tipcalc;
 
+
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-        import javafx.scene.control.Button;
-        import javafx.scene.control.Slider;
-        import javafx.scene.control.TextField;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
-import java.math.BigDecimal;
+
 
 public class tipCalcController {
 
+    private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percent = NumberFormat.getPercentInstance();
+
+    private BigDecimal tipPercentage = new BigDecimal(0.15);
+
+
+
+
+    @FXML
+    public void initialize() {
+        // 0-4 rounds down, 5-9 rounds up
+        currency.setRoundingMode(RoundingMode.HALF_UP);
+
+        tipPercentageSlider.valueProperty().addListener(new ChangeListener<Number>()
+        {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> ov,
+                                        Number oldValue, Number newValue) {
+                        tipPercentage =
+                                BigDecimal.valueOf(newValue.intValue() / 100.0);
+                        tipPercentageLabel.setText(percent.format(tipPercentage));
+                    }
+ }
+                );
+    }
+
+
+
+
+
 
 
 
     @FXML
-    private TextField Amount;
+    private TextField amountTextField;
 
     @FXML
-    private Slider Slider;
+
+    private Label tipPercentageLabel;
 
     @FXML
-    private Button calculate;
+    private Slider tipPercentageSlider;
 
     @FXML
-    private TextField tip;
+    private TextField tipTextField;
 
     @FXML
-    private TextField total;
+    private TextField totalTextField;
+
+    @FXML
+    private void calculateButtonPressed(ActionEvent event) {
+        try {
+            BigDecimal amount = new BigDecimal(amountTextField.getText());
+            BigDecimal tip = amount.multiply(tipPercentage);
+            BigDecimal total = amount.add(tip);
+
+            tipTextField.setText(currency.format(tip));
+            totalTextField.setText(currency.format(total));
+        }
+        catch (NumberFormatException ex) {
+            amountTextField.setText("Enter amount");
+            amountTextField.selectAll();
+            amountTextField.requestFocus();
+        }
+    }
 
 }
