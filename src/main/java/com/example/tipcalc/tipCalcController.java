fcdacb20b2b3c6cef669c2ea181c245d5a6aab14
SW <sunwar01@easv.dk>
2022-10-05 15:06:02 +0200

    final

--- src/main/java/com/example/tipcalc/tipCalcController.java
+++ src/main/java/com/example/tipcalc/tipCalcController.java
@@ -1,5 +1,7 @@
 package com.example.tipcalc;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -8,20 +10,26 @@ import javafx.scene.control.Label;
         import javafx.scene.control.TextField;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
 
 public class tipCalcController {
 
-    @FXML
-    private void calculateButtonPressed(ActionEvent event){
+
+private static final NumberFormat currency =
+NumberFormat.getCurrencyInstance();
+ private static final NumberFormat percent =
+NumberFormat.getPercentInstance();
+
+private BigDecimal tipPercentage = new BigDecimal(0.15); // 15% defaul
 
 
-    }
+
 
     @FXML
     private TextField amountTextField;
 
-    @FXML
-    private Button calculateButton;
+
 
     @FXML
     private Label tipPercentageLabel;
@@ -34,4 +42,43 @@ public class tipCalcController {
 
     @FXML
     private TextField totalTextField;
+
+
+
+ public void initialize() {
+         // 0-4 rounds down, 5-9 rounds up
+         currency.setRoundingMode(RoundingMode.HALF_UP);
+
+         // listener for changes to tipPercentageSlider's value
+         tipPercentageSlider.valueProperty().addListener(
+                 new ChangeListener<Number>() {
+ @Override
+ public void changed(ObservableValue<? extends Number> ov,
+                     Number oldValue, Number newValue) {
+                 tipPercentage =
+                         BigDecimal.valueOf(newValue.intValue() / 100.0);
+                 tipPercentageLabel.setText(percent.format(tipPercentage));}
+ }
+         );
+         }
+
+
+
+    @FXML
+    private void calculateButtonPressed(ActionEvent event) {
+        try {
+            BigDecimal amount = new BigDecimal(amountTextField.getText());
+            BigDecimal tip = amount.multiply(tipPercentage);
+            BigDecimal total = amount.add(tip);
+
+            tipTextField.setText(currency.format(tip));
+            totalTextField.setText(currency.format(total));
+        }
+        catch (NumberFormatException ex) {
+            amountTextField.setText("Enter amount");
+            amountTextField.selectAll();
+            amountTextField.requestFocus();
+        }
+    }
+
 }
